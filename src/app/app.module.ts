c542dc049eb899cce5e6e38211d9a4ac0a8a399d
Edward Moron <edwaramor9@gmail.com>
2019-05-26 23:40:18 -0500

    parte de edward agregada a angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { NavbarComponent } from "./navbar/navbar.component";
+import { PerfilComponent } from "./perfil/perfil.component";
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule
